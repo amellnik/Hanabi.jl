a7dd5fdbf4d1298e01793a97792a07cefd9386d3
Alex Mellnik <a.r.mellnik@gmail.com>
2017-09-14 21:07:52 -0700

    ObservedCards and hints

--- src/Hanabi.jl
+++ src/Hanabi.jl
@@ -1,5 +1,9 @@
 module Hanabi
 
+using Reexport
+@reexport using Nulls
+
+
 ### Suite type
 export Suite
 mutable struct Suite
@@ -43,6 +47,32 @@ Base.show(io::IO, c::Card) = print(io, c.suite , " ", c.value )
 
 # Drawn cards are unhinted
 Card(suite::Suite, value::Int) = Card(suite::Suite, value, false, false)
+
+# Provide information about a card to the player holding it
+export hint!
+function hint!(c::Card, property::String)
+    if !(property in ["suite", "value"])
+        error("You need to give hints about either the suite or value")
+    end
+    if property == "suite"
+        c.suite_known = true
+    else
+        c.value_known = true
+    end
+    return c
+end
+###
+
+### ObservedCard type
+export ObservedCard
+mutable struct ObservedCard
+    suite::Union{Suite, Null}
+    value::Union{Int, Null}
+end
+Base.show(io::IO, c::ObservedCard) = print(io, isnull(c.suite) ? "?" : c.suite, " ", isnull(c.value) ? "?" : c.value)
+
+export observe
+observe(c::Card) = ObservedCard(c.suite_known ? c.suite : null, c.value_known ? c.value : null)
 ###
 
 ### Hand type
